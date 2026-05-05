e6b2fd087ad469897774325eec09d4cc3446a644
sifatrabbioffice <sifatrabbioffice@gmail.com>
2026-05-05 17:21:21 +0600

    Create GameHubApp.swift

---
+++ GameHub/GameHubApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct GameHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // This assumes you have a ContentView; 
+            // if your main view has a different name, update it here.
+            ContentView() 
+        }
+    }
+}
